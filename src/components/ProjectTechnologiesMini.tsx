4da708a72a0657b8e6ece721d1135281bc706a58
SimaoNevesCurado <simao.curadoo@gmail.com>
2024-12-03 15:20:10 +0000

    ooo

--- src/components/ProjectTechnologiesMini.tsx
+++ src/components/ProjectTechnologiesMini.tsx
@@ -1,18 +1,17 @@
 "use client";
-
-const ProjectTechnologiesMini = ({ techStack }: { techStack: (string | undefined)[] }) => {
+const ProjectTechnologiesMini = ({ techStack }: { techStack: string[] }) => {
     return (
       <div className="flex flex-wrap items-center gap-2 max-w-sm">
-        {techStack
-          .filter((tech): tech is string => !!tech) 
-          .map((tech) => (
-            <p
-              key={tech}
-              className="text-[10px] text-dark-200/60 hover:text-dark-200 dark:text-white/60 dark:hover:bg-neutral-600 font-medium bg-neutral-900 dark:bg-dark-300 hover:bg-dark-300/10 dark:hover:bg-dark-400 border dark:border-neutral-600/40 transition-colors duration-200 w-fit rounded-md py-1 px-3"
-            >
-              {tech}
-            </p>
-          ))}
+        {techStack.map((tech) => (
+          <p
+            key={tech}
+            className="text-[10px] text-dark-200/60 hover:text-dark-200 dark:text-white/60 dark:hover:bg-neutral-600 font-medium bg-neutral-900 dark:bg-dark-300 hover:bg-dark-300/10 dark:hover:bg-dark-400 border dark:border-neutral-600/40 transition-colors duration-200 w-fit rounded-md py-1 px-3"
+          >
+            {tech}
+          </p>
+        ))}
       </div>
     );
   };
+
+  export default ProjectTechnologiesMini;
